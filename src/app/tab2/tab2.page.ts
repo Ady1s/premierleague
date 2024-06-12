ee5d8c203f2780d0c098f94f9d6636588ed6c202
Ady1s <adamkunorza@seznam.cz>
2024-06-12 21:39:34 +0200

    commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Observable, of, tap } from 'rxjs';
+import { Player } from '../models/player.model';
+import { PlayerService } from '../services/player.service';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,30 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  @ViewChild(IonModal) modal!: IonModal
 
+  players2$: Observable<Player[]>
+
+  private playersOrigin2: Player[] = []
+
+  constructor(private playerService2: PlayerService) {
+    this.players2$ = this.playerService2.index$()
+        .pipe(tap(players=> {
+          this.playersOrigin2 = players
+        }))
+  }
+
+  transferPlayer2(player:Player){
+    this.playerService2.player = player
+  }
+
+  searchPlayer(event: CustomEvent) {
+    const value = event.detail.value.toLowerCase()
+
+    const filteredPlayers = this.playersOrigin2.filter(player => {
+      return player.name.display.toLowerCase().includes(value);
+    })
+
+    this.players2$ = of(filteredPlayers);
+  }
 }
