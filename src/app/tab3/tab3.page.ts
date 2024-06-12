e81c74eb81827ca00e38ce6ec17d8b6210ff9014
Ady1s <adamkunorza@seznam.cz>
2024-06-12 22:00:09 +0200

    commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Observable, of, tap } from 'rxjs';
+import { Player } from '../models/player.model';
+import { PlayerService } from '../services/player.service';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  @ViewChild(IonModal) modal!: IonModal
 
+  players3$: Observable<Player[]>
+
+  private playersOrigin3: Player[] = []
+
+  constructor(private playerService3: PlayerService) {
+    this.players3$ = this.playerService3.index$()
+        .pipe(tap(players=> {
+          this.playersOrigin3 = players
+        }))
+  }
+
+  transferPlayer3(player:Player){
+    this.playerService3.player = player
+  }
+
+  searchPlayer(event: CustomEvent) {
+    const value = event.detail.value.toLowerCase()
+
+    const filteredPlayers = this.playersOrigin3.filter(player => {
+      return player.name.display.toLowerCase().includes(value);
+    })
+
+    this.players3$ = of(filteredPlayers);
+    }
 }
